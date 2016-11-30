6609430cf4ebe05a2f0674770340097a83205776
Sergiu M <sergiu.munteanu@live.com>
2016-11-30 23:27:16 +0100

    Added swagger for /api/reports

--- app/Api/V1/Controllers/ReportsController.php
+++ app/Api/V1/Controllers/ReportsController.php
@@ -13,6 +13,33 @@ class ReportsController extends Controller
     
     use Helpers;
 
+    /**
+    * @SWG\Get(
+    *     path="/api/reports",
+    *     summary="Fetch report summary",
+    *     tags={"Reports"},
+    *     description="Fetch rerpots summary.",
+    *     operationId="fetchReportsSummary",
+    *     consumes={"application/json"},
+    *     produces={"application/json"},
+    *     @SWG\Response(
+    *         response=200,
+    *         description="successful operation",
+    *         @SWG\Schema(
+    *             type="array"
+    *         ),
+    *     ),
+    *     @SWG\Response(
+    *         response="400",
+    *         description="Invalid tag value",
+    *     ),
+    *     security={
+    *         {
+    *             "monitorizare_auth": {"read:reports"}
+    *         }
+    *     }
+    * )
+    */
     public function index()
     {   
         return response()->json([
