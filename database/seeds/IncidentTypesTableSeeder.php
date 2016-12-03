ec0e41bce52a96631e258ce583ecfe6f11b84e70
Costea Alexandru <costeaalex@yahoo.com>
2016-12-03 13:41:09 +0200

    Added codes to all the incident types.

--- database/seeds/IncidentTypesTableSeeder.php
+++ database/seeds/IncidentTypesTableSeeder.php
@@ -13,15 +13,15 @@ class IncidentTypesTableSeeder extends Seeder
     public function run()
     {
         IncidentType::create(['id' => 1, 'code' => 'OTH', 'name' => 'Altul' ]);
-        IncidentType::create(['id' => 2, 'code' => '', 'name' => 'Campanie electorală în ziua votului' ]);
-        IncidentType::create(['id' => 3, 'code' => '', 'name' => 'Media & internet' ]);
-        IncidentType::create(['id' => 4, 'code' => '', 'name' => 'Mită electorală' ]);
-        IncidentType::create(['id' => 5, 'code' => '', 'name' => 'Nereguli în funcționarea birourilor electorale' ]);
-        IncidentType::create(['id' => 6, 'code' => '', 'name' => 'Observatori acreditați' ]);
-        IncidentType::create(['id' => 7, 'code' => '', 'name' => 'Probleme legate de observatorii acreditați' ]);
-        IncidentType::create(['id' => 8, 'code' => '', 'name' => 'Turism electoral' ]);
-        IncidentType::create(['id' => 9, 'code' => '', 'name' => 'Utilizarea fondurilor publice în scopuri electorale' ]);
-        IncidentType::create(['id' => 10, 'code' => '', 'name' => 'Vot multiplu' ]);
+        IncidentType::create(['id' => 2, 'code' => 'ELE', 'name' => 'Campanie electorală în ziua votului' ]);
+        IncidentType::create(['id' => 3, 'code' => 'MED', 'name' => 'Media & internet' ]);
+        IncidentType::create(['id' => 4, 'code' => 'MIT', 'name' => 'Mită electorală' ]);
+        IncidentType::create(['id' => 5, 'code' => 'NBE', 'name' => 'Nereguli în funcționarea birourilor electorale' ]);
+        IncidentType::create(['id' => 6, 'code' => 'ACC', 'name' => 'Observatori acreditați' ]);
+        IncidentType::create(['id' => 7, 'code' => 'OBP', 'name' => 'Probleme legate de observatorii acreditați' ]);
+        IncidentType::create(['id' => 8, 'code' => 'TEL', 'name' => 'Turism electoral' ]);
+        IncidentType::create(['id' => 9, 'code' => 'FEL', 'name' => 'Utilizarea fondurilor publice în scopuri electorale' ]);
+        IncidentType::create(['id' => 10, 'code' => 'VML', 'name' => 'Vot multiplu' ]);
         IncidentType::create(['id' => 11, 'code' => 'OPN', 'name' => 'Deschidere sectie' ]);
         IncidentType::create(['id' => 12, 'code' => 'NUM', 'name' => 'Numarare' ]);
     }
