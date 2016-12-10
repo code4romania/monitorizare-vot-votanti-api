7ef92b70d7d2adc6f94396049d5ae4c68c15d25b
Costea Alexandru <costeaalex@yahoo.com>
2016-12-10 13:33:26 +0200

    Additional field with address for Diaspora Precincts.

--- app/Api/V1/Transformers/PrecinctTransformer.php
+++ app/Api/V1/Transformers/PrecinctTransformer.php
@@ -2,16 +2,26 @@
 
 namespace App\Api\V1\Transformers;
 
+use App\County;
+
 class PrecinctTransformer extends Transformer
 {
     public function transform($precinct)
     {
+        $countyAbroad = County::where('code', 'DI')->first();
+        $countyAbroadId = $countyAbroad->id;
+        $additional = '';
+        if($countyAbroadId == $precinct['county_id']) {
+            $additional = $precinct->address;
+        }
+        
         return [
                  
                 'id' => $precinct['id'],
                 'county' => $precinct['county_id'],
                 'city' => $precinct['city_id'],
-                'precinctNo' => $precinct['precinct_no']
+                'precinctNo' => $precinct['precinct_no'],
+                'additional' => $additional
         ];
     }
 }
