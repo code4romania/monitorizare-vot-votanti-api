55585da20989a86e5adcc06d5c736c07a72f627f
Costea Alexandru <costeaalex@yahoo.com>
2018-09-26 11:21:02 +0300

    Update IncidentTypesTableSeeder.php

--- database/seeds/IncidentTypesTableSeeder.php
+++ database/seeds/IncidentTypesTableSeeder.php
@@ -12,71 +12,65 @@ class IncidentTypesTableSeeder extends Seeder
 	 */
 	public function run()
 	{
-		IncidentType::create([
-				'id' => 1,
-				'label' => 'IT_OTHER',
-				'code' => 'OTH',
-				'name' => 'Altul'
-		]);
-		IncidentType::create([
-				'id' => 2,
-				'label' => 'IT_ELECTION_DAY',
-				'code' => 'ELE',
-				'name' => 'Campanie electorală în ziua votului'
-		]);
-		IncidentType::create([
-				'id' => 3,
-				'label' => 'IT_MEDIA',
-				'code' => 'MED',
-				'name' => 'Media & internet'
-		]);
-		IncidentType::create([
-				'id' => 4,
-				'label' => 'IT_BRIBE',
-				'code' => 'MIT',
-				'name' => 'Mită electorală'
-		]);
-		IncidentType::create([
-				'id' => 5,
-				'label' => 'IT_OFFICES',
-				'code' => 'NBE',
-				'name' => 'Nereguli în funcționarea birourilor electorale'
-		]);
-		IncidentType::create([
-				'id' => 7,
-				'label' => 'IT_OBSERVERS',
-				'code' => 'OBP',
-				'name' => 'Probleme legate de observatorii acreditați'
-		]);
-		IncidentType::create([
-				'id' => 8,
-				'label' => 'IT_ELEC_TURISM',
-				'code' => 'TEL',
-				'name' => 'Turism electoral'
-		]);
-		IncidentType::create([
-				'id' => 9,
-				'label' => 'IT_PUBLIC_FOUNDS',
-				'code' => 'FEL',
-				'name' => 'Utilizarea fondurilor publice în scopuri electorale'
-		]);
-		IncidentType::create([
-				'id' => 10,
-				'label' => 'IT_MULTIPLE',
-				'code' => 'VML',
-				'name' => 'Vot multiplu'
-		]);
-		IncidentType::create([
-				'id' => 11,
-				'label' => 'IT_OPENING',
-				'code' => 'OPN',
-				'name' => 'Deschidere sectie'
-		]);
-		IncidentType::create([
-				'id' => 12,
-				'label' => 'IT_COUNTING',
-				'code' => 'NUM',
-				'name' => 'Numarare'
-		]);
+	       IncidentType::create([
+               'id' => 1,
+               'label' => 'IT_OTHER',
+               'code' => 'OTH',
+               'name' => 'Altul'
+	       ]);
+	       IncidentType::create([
+		       'id' => 2,
+		       'label' => 'IT_OBSERVERS',
+		       'code' => 'ELE',
+		       'name' => 'Probleme legate de campania electorală'
+	       ]);
+	       IncidentType::create([
+		       'id' => 3,
+		       'label' => 'IT_MEDIA',
+		       'code' => 'MED',
+		       'name' => 'Discurs instigator la ură'
+	       ]);
+	       IncidentType::create([
+		       'id' => 4,
+		       'label' => 'IT_BRIBE',
+		       'code' => 'MIT',
+		       'name' => 'Vot multiplu/Mită electorală'
+	       ]);
+	       IncidentType::create([
+		       'id' => 5,
+		       'label' => 'BUILDING',
+		       'code' => 'NBE',
+		       'name' => 'Nereguli legate de funcționarea administrației electorale'
+	       ]);
+	       IncidentType::create([
+		       'id' => 7,
+		       'label' => 'LOCATION',
+		       'code' => 'OBP',
+		       'name' => 'Vot în străinătate'
+	       ]);
+	       IncidentType::create([
+		       'id' => 8,
+		       'label' => 'IT_ELEC_TURISM',
+		       'code' => 'TEL',
+		       'name' => 'Transport organizat de alegători'
+	       ]);
+	       IncidentType::create([
+		       'id' => 9,
+		       'label' => 'IT_PUBLIC_FOUNDS',
+		       'code' => 'FEL',
+		       'name' => 'Abuz de resurse publice'
+	       ]);
+	       IncidentType::create([
+		       'id' => 11,
+		       'label' => 'POLICE',
+		       'code' => 'OPN',
+		       'name' => 'Presiuni din partea autorităților locale'
+	       ]);
+	       IncidentType::create([
+		       'id' => 12,
+		       'label' => 'VOTE',
+		       'code' => 'NUM',
+		       'name' => 'Probleme în zilele votului'
+	       ]);
 	}
 }
